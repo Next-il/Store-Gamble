f4e95985d69d0db3f9b63321c14ea3451b3f2e5c
Amir Eliz <tripl3n2@gmail.com>
2025-08-08 04:50:09 +0300

    Remove minimum and add "all" option

--- cs2-store-gamble.cs
+++ cs2-store-gamble.cs
@@ -59,7 +59,7 @@ public class Store_Gamble : BasePlugin, IPluginConfig<Store_GambleConfig>
 	public void OnConfigParsed(Store_GambleConfig config)
 	{
 		config.MinCredits = Math.Max(0, config.MinCredits);
-		config.MaxCredits = Math.Max(config.MinCredits + 1, config.MaxCredits);
+		config.MaxCredits = config.MaxCredits > 0 ? Math.Max(config.MinCredits + 1, config.MaxCredits) : 0;
 
 		config.WinChance = Math.Clamp(config.WinChance, 1, 100);
 
@@ -95,10 +95,18 @@ public class Store_Gamble : BasePlugin, IPluginConfig<Store_GambleConfig>
 			return;
 		}
 
-		if (!int.TryParse(info.GetArg(1), out int credits))
+		// If not int or "all", reply with error
+		if (!int.TryParse(info.GetArg(1), out int credits) || credits <= 0)
 		{
-			info.ReplyToCommand(Localizer["Prefix"] + Localizer["Must be an integer"]);
-			return;
+			if (info.GetArg(1).ToLower() == "all")
+			{
+				credits = StoreApi.GetPlayerCredits(player);
+			}
+			else
+			{
+				info.ReplyToCommand(Localizer["Prefix"] + Localizer["Must be an integer"]);
+				return;
+			}
 		}
 
 		if (StoreApi.GetPlayerCredits(player) < credits)
@@ -113,7 +121,7 @@ public class Store_Gamble : BasePlugin, IPluginConfig<Store_GambleConfig>
 			return;
 		}
 
-		if (credits > Config.MaxCredits)
+		if (Config.MaxCredits > 0 && credits > Config.MaxCredits)
 		{
 			info.ReplyToCommand(Localizer["Prefix"] + Localizer["Max gamble", Config.MaxCredits]);
 			return;
@@ -125,7 +133,7 @@ public class Store_Gamble : BasePlugin, IPluginConfig<Store_GambleConfig>
 		var win = Random.Next(1, 101) <= Config.WinChance;
 		var winCredits = win ? credits : -credits;
 
-		StoreApi.GivePlayerCredits(player, winCredits);
+		StoreApi.GivePlayerCredits(player, winCredits, "Gamble");
 
 		Server.PrintToChatAll(Localizer["Prefix"] + Localizer[win ? "Win" : "Lose", player.PlayerName, credits]);
 	}
